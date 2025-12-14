b18d15b2b97711c665bf4775e941c13d19ac0fa5
ItaloMatheus10 <italomrs2345@gmail.com>
2025-12-14 13:20:22 -0300

    Criação da classe LoxInstance

---
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/LoxInstance.java
@@ -0,0 +1,16 @@
+package br.com.analisadorlexico;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class LoxInstance {
+  private LoxClass klass;
+
+  LoxInstance(LoxClass klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String toString() {
+    return klass.name + " instance";
+  }
