6ad8ff5bccc766603595aade7c4c70b78f250a7c
ItaloMatheus10 <italomrs2345@gmail.com>
2025-12-14 11:31:00 -0300

    Adição do visitVarStmt

--- analisador-lexico/src/main/java/br/com/analisadorlexico/Resolver.java
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/Resolver.java
@@ -15,12 +15,22 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 }
 
 @Override
-  public Void visitBlockStmt(Stmt.Block stmt) {
+public Void visitBlockStmt(Stmt.Block stmt) {
     beginScope();
     resolve(stmt.statements);
     endScope();
     return null;
 }
+
+@Override
+public Void visitVarStmt(Stmt.Var stmt) {
+    declare(stmt.name);
+    if (stmt.initializer != null) {
+      resolve(stmt.initializer);
+    }
+    define(stmt.name);
+    return null;
+}  
 
 void resolve(List<Stmt> statements) {
     for (Stmt statement : statements) {
@@ -38,4 +48,8 @@ private void resolve(Expr expr) {
 
 private void beginScope() {
     scopes.push(new HashMap<String, Boolean>());
+}
+
+private void endScope() {
+    scopes.pop();
 }
