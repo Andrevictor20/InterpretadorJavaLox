efe1db2edf3daca4783e8fd10736fab167d17410
ItaloMatheus10 <italomrs2345@gmail.com>
2025-12-14 11:46:02 -0300

    Resolvendo os nós de uma árvore

--- analisador-lexico/src/main/java/br/com/analisadorlexico/Resolver.java
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/Resolver.java
@@ -23,11 +23,40 @@ public Void visitBlockStmt(Stmt.Block stmt) {
 }
 
 @Override
+public Void visitExpressionStmt(Stmt.Expression stmt) {
+    resolve(stmt.expression);
+    return null;
+}
+
+@Override
 public Void visitFunctionStmt(Stmt.Function stmt) {
     declare(stmt.name);
     define(stmt.name);
 
     resolveFunction(stmt);
+    return null;
+}
+
+@Override
+public Void visitIfStmt(Stmt.If stmt) {
+    resolve(stmt.condition);
+    resolve(stmt.thenBranch);
+    if (stmt.elseBranch != null) resolve(stmt.elseBranch);
+    return null;
+}
+
+@Override
+public Void visitPrintStmt(Stmt.Print stmt) {
+    resolve(stmt.expression);
+    return null;
+}
+
+@Override
+public Void visitReturnStmt(Stmt.Return stmt) {
+    if (stmt.value != null) {
+      resolve(stmt.value);
+    }
+
     return null;
 }
 
@@ -40,6 +69,13 @@ public Void visitVarStmt(Stmt.Var stmt) {
     define(stmt.name);
     return null;
 }
+
+@Override
+public Void visitWhileStmt(Stmt.While stmt) {
+    resolve(stmt.condition);
+    resolve(stmt.body);
+    return null;
+}  
 
 @Override
 public Void visitVariableExpr(Expr.Variable expr) {
@@ -57,6 +93,48 @@ public Void visitVariableExpr(Expr.Variable expr) {
 public Void visitAssignExpr(Expr.Assign expr) {
     resolve(expr.value);
     resolveLocal(expr, expr.name);
+    return null;
+}
+
+@Override
+public Void visitBinaryExpr(Expr.Binary expr) {
+    resolve(expr.left);
+    resolve(expr.right);
+    return null;
+}
+
+@Override
+public Void visitCallExpr(Expr.Call expr) {
+    resolve(expr.callee);
+
+    for (Expr argument : expr.arguments) {
+      resolve(argument);
+    }
+
+    return null;
+}
+
+@Override
+public Void visitGroupingExpr(Expr.Grouping expr) {
+    resolve(expr.expression);
+    return null;
+}
+
+@Override
+public Void visitLogicalExpr(Expr.Logical expr) {
+    resolve(expr.left);
+    resolve(expr.right);
+    return null;
+}
+
+@Override
+public Void visitUnaryExpr(Expr.Unary expr) {
+    resolve(expr.right);
+    return null;
+}
+
+@Override
+public Void visitLiteralExpr(Expr.Literal expr) {
     return null;
 }
 
